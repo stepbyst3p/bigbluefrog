4d16918da93a97a4bbc2dd0848c7c225addc9ab7
Ivan Sergeev <spikerdn@gmail.com>
2018-02-13 18:43:31 +0300

    added countdown timer, photo carousel, reworked texts and styles

--- js/main.js
+++ js/main.js
@@ -8,3 +8,24 @@ $(document).on("scroll", function () {
         $("nav").addClass("transparent");
     }
 });
+
+$(document).ready(function () {
+    $('.team-photo-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false
+    });
+    $('.btn-slider-left').click(function () {
+        $('.team-photo-slider').slick('slickPrev');
+    })
+
+    $('.btn-slider-right').click(function () {
+        $('.team-photo-slider').slick('slickNext');
+    })
+});
+
+
+$('#clock').countdown('2018/03/10', function (event) {
+    $(this).html(event.strftime('%D дня %H:%M:%S'));
+});
